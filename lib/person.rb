24c4a1d661d9ac01c431470194fadb879d4a54ff
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-14 13:57:53 +0300

    completed metaprogramming

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do|key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+bob = Person.new(bob_attributes)
+puts bob.name       # => "Bob"
+puts bob.hair_color # => "Brown"
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+puts susan.name      # => "Susan"
+puts susan.height    # => "5'11""
+puts susan.eye_color # => "Green"
